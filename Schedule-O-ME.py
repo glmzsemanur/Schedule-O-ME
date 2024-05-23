9b46b6a66c1b1b97f1d66e20431e39ec0ee8d253
glmzsemanur <147614200+glmzsemanur@users.noreply.github.com>
2024-05-23 09:29:27 +0300

    Update Schedule-O-ME.py

--- Schedule-O-ME.py
+++ Schedule-O-ME.py
@@ -4,7 +4,9 @@ import functions
 import subprocess
 
 
-# First start menu
+# First start menu, checks if scraped data is present or not.
+# If data is not available, asks for permission to download it.
+# If the data is available, proceeds with the main menu.
 print("WELCOME TO Schedule-O-ME.")
 folder_name = "scraped_data"
 while True:
@@ -26,12 +28,17 @@ while True:
                   "proceed with updating the data.")
 raw_file = f"scraped_data\\{file}.xlsx"
 
+# Right before Main Menu, the data is read and formats/edits for better use.
 df = pd.read_excel(raw_file).fillna("---")
-df = functions.fix_instructor(df)
+
+# Removes double spaces between words.
+df = functions.fix_double_space(df)
+
+# Splits lectures with multiple sections if any, and saves it as another dataframe.
 df_by_sections = functions.split_sections(df)
-#df_by_sections.to_excel("a.xlsx", index=False)
 
 # Main menu after first start
+# Prints out the Main Menu and runs the corresponding function to the user's input.
 while True:
     print("MAIN MENU")
     print("1. Find-ME an empty classroom.")
@@ -45,25 +52,24 @@ while True:
         exit("User terminated the application")
     elif inp == "1":
         functions.find_empty(df_by_sections)
-        input("Press enter to continue with Main Menu.")
     elif inp == "2":
-        functions.crn(df, input("Enter a CRN: "))
-        input("Press enter to continue with Main Menu.")
+        functions.search_crn(df, input("Enter a CRN: "))
     elif inp == "3":
         functions.find_schedule(df_by_sections, df)
-        input("Press enter to continue with Main Menu.")
     elif inp == "4":
         if input("Are you sure? Updating will take couple of minutes. Y/N? ").upper() == "Y":
             try:
                 functions.get_raw_data()
                 df.to_excel("a.xlsx", index=False)
-            except :
-                print("Cannot update without internet connection.")
-        input("Press enter to continue with Main Menu.")
+            except Exception as e:
+                print("Cannot update without internet connection.", e)
     elif inp == "5":
-        result = subprocess.run(["start", "README.md"], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.run(["start", "README.md"], shell=True, stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
         if result.returncode == 0:
             print("Read-ME file has been opened successfully.")
         else:
             print("Cannot open file, you may find the Read-ME.md file in the current directory.")
-        input("Press enter to continue with Main Menu.")
+    else:
+        print("Invalid choice.")
+    input("Press enter to continue with Main Menu.")
